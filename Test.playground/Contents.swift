6db849cbac1b94a1bc7c8b885c696988a184ffd8
Mohammed Abbas <abbas.a.mohammed95@gmail.com>
2022-06-24 06:02:00 +0530

    first commit

---
+++ Test.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Foundation
+
+class ViewController:UIViewController{
+    // create a new screen with 2 textboxes
+    //in a VC these would be outlets connecting to textfield
+    @IBOutlet weak var fromAcctTextField:UITextField!
+    @IBOutlet weak var toAcctTextFIeld:UITextField!
+    
+    var transferAmt:String {
+        return fromAcctTextField.text!
+    }
+    
+    var floatAmount: Float {
+        return Float(transferAmt)!
+    }
+    
+    @IBAction func transferFunds(_ sender:Any) {
+        //inside here we have the logic to take the input from textfield to transfer
+        
+    }
+}
